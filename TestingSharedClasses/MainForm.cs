1db79ff98d8ef5dd86c2b2d5550040c06c56b2a4
francoishill <francoishill@d35661ca-e1af-0410-899d-dbc427243e09>
2011-10-28 10:46:39 +0000

    Converted to work with own serialization as it does not work right with built in C# serialization when working with two different applications (assemblies). Can also now (de)serialize to binary/xml. Types currently supported: string, int, long, bool, double, List<string>.
    git-svn-id: svn://localhost/testingsharedclasses/trunk@24 d35661ca-e1af-0410-899d-dbc427243e09

--- TestingSharedClasses/MainForm.cs
+++ TestingSharedClasses/MainForm.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
@@ -42,6 +43,20 @@ namespace TestingSharedClasses
 			NetworkingInteropForm networkingInteropForm = new NetworkingInteropForm();
 			networkingInteropForm.Show();
 			networkingInteropForm.buttonServer.PerformClick();
+
+			//MemoryStream stream = new MemoryStream();
+			//BinaryWriter b = new BinaryWriter(stream);
+			////b.Write(true);
+			//b.Write("F");
+			//stream.Flush();
+			//b.Write(123.1234);
+			//b.Write(456D);
+			//stream.Position = 0;
+			//BinaryReader br = new BinaryReader(stream);
+			//MessageBox.Show(br.ReadBoolean().ToString());
+			//MessageBox.Show(br.ReadString());
+			//MessageBox.Show(br.ReadDouble().ToString());
+			//MessageBox.Show(br.ReadDouble().ToString());
 		}
 	}
 }
