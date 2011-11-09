1943cf58bced3abdb970cf996f4a35a8ce3af340
francoishill <francoishill@d35661ca-e1af-0410-899d-dbc427243e09>
2011-11-09 22:25:13 +0000

    Couple of changes for TestXmlRpc.
    git-svn-id: svn://localhost/testingsharedclasses/trunk@34 d35661ca-e1af-0410-899d-dbc427243e09

--- TestingSharedClasses/TestXmlRpc.Designer.cs
+++ TestingSharedClasses/TestXmlRpc.Designer.cs
@@ -30,6 +30,18 @@
 		{
 			this.button_XmlRpcClient = new System.Windows.Forms.Button();
 			this.button_XmlRpcServer = new System.Windows.Forms.Button();
+			this.button_TestTracXmlRpc = new System.Windows.Forms.Button();
+			this.dataGridView_Tickets = new System.Windows.Forms.DataGridView();
+			this.dataGridView_ChangeLog = new System.Windows.Forms.DataGridView();
+			this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+			this.KeyColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.ValueColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView_Tickets)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView_ChangeLog)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+			this.splitContainer1.Panel1.SuspendLayout();
+			this.splitContainer1.Panel2.SuspendLayout();
+			this.splitContainer1.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// button_XmlRpcClient
@@ -52,16 +64,87 @@
 			this.button_XmlRpcServer.UseVisualStyleBackColor = true;
 			this.button_XmlRpcServer.Click += new System.EventHandler(this.button_XmlRpcServer_Click);
 			// 
+			// button_TestTracXmlRpc
+			// 
+			this.button_TestTracXmlRpc.Location = new System.Drawing.Point(12, 70);
+			this.button_TestTracXmlRpc.Name = "button_TestTracXmlRpc";
+			this.button_TestTracXmlRpc.Size = new System.Drawing.Size(106, 23);
+			this.button_TestTracXmlRpc.TabIndex = 3;
+			this.button_TestTracXmlRpc.Text = "Test Trac XmlRpc";
+			this.button_TestTracXmlRpc.UseVisualStyleBackColor = true;
+			this.button_TestTracXmlRpc.Click += new System.EventHandler(this.button_TestTracXmlRpc_Click);
+			// 
+			// dataGridView_Tickets
+			// 
+			this.dataGridView_Tickets.AllowUserToAddRows = false;
+			this.dataGridView_Tickets.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView_Tickets.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.dataGridView_Tickets.Location = new System.Drawing.Point(0, 0);
+			this.dataGridView_Tickets.Name = "dataGridView_Tickets";
+			this.dataGridView_Tickets.Size = new System.Drawing.Size(524, 372);
+			this.dataGridView_Tickets.TabIndex = 4;
+			this.dataGridView_Tickets.RowEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView_Tickets_RowEnter);
+			// 
+			// dataGridView_ChangeLog
+			// 
+			this.dataGridView_ChangeLog.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView_ChangeLog.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.KeyColumn,
+            this.ValueColumn});
+			this.dataGridView_ChangeLog.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.dataGridView_ChangeLog.Location = new System.Drawing.Point(0, 0);
+			this.dataGridView_ChangeLog.Name = "dataGridView_ChangeLog";
+			this.dataGridView_ChangeLog.Size = new System.Drawing.Size(258, 372);
+			this.dataGridView_ChangeLog.TabIndex = 5;
+			// 
+			// splitContainer1
+			// 
+			this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel2;
+			this.splitContainer1.Location = new System.Drawing.Point(13, 117);
+			this.splitContainer1.Name = "splitContainer1";
+			// 
+			// splitContainer1.Panel1
+			// 
+			this.splitContainer1.Panel1.Controls.Add(this.dataGridView_Tickets);
+			// 
+			// splitContainer1.Panel2
+			// 
+			this.splitContainer1.Panel2.Controls.Add(this.dataGridView_ChangeLog);
+			this.splitContainer1.Size = new System.Drawing.Size(786, 372);
+			this.splitContainer1.SplitterDistance = 524;
+			this.splitContainer1.TabIndex = 6;
+			// 
+			// KeyColumn
+			// 
+			this.KeyColumn.HeaderText = "KeyColumn";
+			this.KeyColumn.Name = "KeyColumn";
+			// 
+			// ValueColumn
+			// 
+			this.ValueColumn.HeaderText = "ValueColumn";
+			this.ValueColumn.Name = "ValueColumn";
+			// 
 			// TestXmlRpc
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(284, 262);
+			this.ClientSize = new System.Drawing.Size(811, 501);
+			this.Controls.Add(this.splitContainer1);
+			this.Controls.Add(this.button_TestTracXmlRpc);
 			this.Controls.Add(this.button_XmlRpcServer);
 			this.Controls.Add(this.button_XmlRpcClient);
 			this.Name = "TestXmlRpc";
 			this.Text = "TestXmlRpc";
 			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.TestXmlRpc_FormClosing);
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView_Tickets)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView_ChangeLog)).EndInit();
+			this.splitContainer1.Panel1.ResumeLayout(false);
+			this.splitContainer1.Panel2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+			this.splitContainer1.ResumeLayout(false);
 			this.ResumeLayout(false);
 
 		}
@@ -70,5 +153,11 @@
 
 		private System.Windows.Forms.Button button_XmlRpcClient;
 		private System.Windows.Forms.Button button_XmlRpcServer;
+		private System.Windows.Forms.Button button_TestTracXmlRpc;
+		private System.Windows.Forms.DataGridView dataGridView_Tickets;
+		private System.Windows.Forms.DataGridView dataGridView_ChangeLog;
+		private System.Windows.Forms.SplitContainer splitContainer1;
+		private System.Windows.Forms.DataGridViewTextBoxColumn KeyColumn;
+		private System.Windows.Forms.DataGridViewTextBoxColumn ValueColumn;
 	}
 }
--- TestingSharedClasses/TestXmlRpc.cs
+++ TestingSharedClasses/TestXmlRpc.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection;
 using System.Runtime.Remoting;
 using System.Runtime.Remoting.Channels;
 using System.Text;
@@ -33,6 +34,101 @@ namespace TestingSharedClasses
 		private void TestXmlRpc_FormClosing(object sender, FormClosingEventArgs e)
 		{
 			XmlRpcInterop.UnregisterAllRegisteredChannels();
+		}
+
+		//TextFeedbackEventHandler textFeedback;
+		private void button_TestTracXmlRpc_Click(object sender, EventArgs e)
+		{
+			ObtainTicketStatusses();
+		}
+
+		private void ObtainTicketStatusses()
+		{
+			button_TestTracXmlRpc.Enabled = false;
+			//textFeedback += (snder, evtargs) =>
+			//{
+			//	MessageBox.Show(evtargs.FeedbackText);
+			//};
+			//VisualStudioInterop.TestTracXmlRpc(textFeedback);
+
+			string url = TracXmlRpcInterop.MonitorSystemXmlRpcUrl;
+
+			dataGridView_ChangeLog.Columns.Clear();
+			foreach (FieldInfo fieldInfo in typeof(TracXmlRpcInterop.ChangeLogStruct).GetFields())
+				dataGridView_ChangeLog.Columns.Add(fieldInfo.Name, fieldInfo.Name);
+
+			dataGridView_Tickets.Columns.Clear();
+			List<string> fieldLabels = null;
+			ThreadingInterop.PerformVoidFunctionSeperateThread(() =>
+			{ fieldLabels = TracXmlRpcInterop.GetFieldLables(url); },
+				ThreadName: "TracXmlRpcInterop GetFieldLabels");
+			foreach (string fieldlabel in fieldLabels)
+				dataGridView_Tickets.Columns.Add(fieldlabel.ToLower(), fieldlabel);
+
+			int[] ticketIdList = new int[0];
+			ThreadingInterop.PerformVoidFunctionSeperateThread(() =>
+			{ ticketIdList = TracXmlRpcInterop.GetTicketIds(url); },
+				ThreadName: "TracXmlRpcInterop GetTicketIds");
+
+			foreach (int ticketID in ticketIdList)
+			{
+				dataGridView_Tickets.Rows.Add();
+				int rowNum = dataGridView_Tickets.Rows.Count - (dataGridView_Tickets.AllowUserToAddRows ? 2 : 1);
+				Dictionary<string, object> dict = null;
+				ThreadingInterop.PerformVoidFunctionSeperateThread(() =>
+				{ dict = TracXmlRpcInterop.GetFieldValuesOfTicket(ticketID, url); },
+					ThreadName: "TracXmlRpcInterop GetFieldValuesOfTicket");
+				foreach (string key in dict.Keys)
+				{
+					string tmpColName = key.ToLower();
+					int colNum = dataGridView_Tickets.Columns[tmpColName].Index;
+					if (!dataGridView_Tickets.Columns.Contains(tmpColName)) continue;
+					dataGridView_Tickets[
+						colNum,
+						rowNum
+						].Value = dict[key];
+				}
+				//List<TracXmlRpcInterop.ChangeLogStruct> cl = TracXmlRpcInterop.ChangeLogs(ticketID);
+				//foreach (TracXmlRpcInterop.ChangeLogStruct c in cl)
+				//	MessageBox.Show(c.Field);
+				ThreadingInterop.PerformVoidFunctionSeperateThread(() =>
+				{ dataGridView_Tickets.Rows[rowNum].Tag = TracXmlRpcInterop.ChangeLogs(ticketID, url); },
+					ThreadName: "TracXmlRpcInterop ChangeLogs",
+					CheckInvokeRequired: true,
+					controlToCheckInvokeRequired: this);
+			}
+
+			if (dataGridView_Tickets.Rows.Count > 0)
+			{
+				dataGridView_Tickets.Rows[0].Selected = true;
+				PopulateChangeLogGrid(0);
+			}
+			button_TestTracXmlRpc.Enabled = true;
+		}
+
+		private void dataGridView_Tickets_RowEnter(object sender, DataGridViewCellEventArgs e)
+		{
+			PopulateChangeLogGrid(e.RowIndex);
+		}
+
+		private void PopulateChangeLogGrid(int rowIndex)
+		{
+			dataGridView_ChangeLog.Rows.Clear();
+			if (rowIndex == -1)
+				return;
+			if (dataGridView_Tickets.Rows[rowIndex].Tag is List<TracXmlRpcInterop.ChangeLogStruct>)
+			{
+				List<TracXmlRpcInterop.ChangeLogStruct> changes = dataGridView_Tickets.Rows[rowIndex].Tag as List<TracXmlRpcInterop.ChangeLogStruct>;
+				foreach (TracXmlRpcInterop.ChangeLogStruct change in changes)
+				{
+					dataGridView_ChangeLog.Rows.Add();
+					int rowNum = dataGridView_ChangeLog.Rows.Count - (dataGridView_ChangeLog.AllowUserToAddRows ? 2 : 1);
+					foreach (FieldInfo fieldInfo in typeof(TracXmlRpcInterop.ChangeLogStruct).GetFields())
+						dataGridView_ChangeLog[
+							dataGridView_ChangeLog.Columns[fieldInfo.Name].Index, rowNum].Value =
+							fieldInfo.GetValue(change);
+				}
+			}
 		}
 	}
 }
