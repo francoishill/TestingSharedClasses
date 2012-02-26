06f998e616c406cb404ec57049619bbec3682442
francoishill <francoishill@d35661ca-e1af-0410-899d-dbc427243e09>
2012-02-26 18:12:31 +0000

    git-svn-id: svn://localhost/testingsharedclasses/trunk@64 d35661ca-e1af-0410-899d-dbc427243e09

--- TestingSharedClasses/PermanentNetworkConnection.cs
+++ TestingSharedClasses/PermanentNetworkConnection.cs
@@ -26,6 +26,11 @@ namespace TestingSharedClasses
 		Server s;
 		private void PermanentNetworkConnection_Load(object sender, EventArgs e)
 		{
+			if (XmlRpcInterop.StartDynamicCodeInvokingServer_XmlRpc())
+				AppendServerMessage("XmlRpc server started.");
+			else
+				AppendServerMessage("Unable to start XmpRpc server.");
+
 			//Check out the following links for async TcpClient:
 			//http://robjdavey.wordpress.com/2011/02/11/asynchronous-tcp-client-example/
 			//or rather (improved of the above)
